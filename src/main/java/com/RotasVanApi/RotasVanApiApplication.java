564f3c5434c726e7758ca5d32e66ae484135d3cb
Matheus Nunes <matheusnunes.sobrinho18@gmail.com>
2023-12-22 19:21:09 -0300

    commit inicial

---
+++ src/main/java/com/RotasVanApi/RotasVanApiApplication.java
@@ -0,0 +1,21 @@
+package com.RotasVanApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RotasVanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotasVanApiApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index(){
+		return "Tudo certo!!";
+	}
+
+}
