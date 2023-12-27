aaa09d8c7acfe3536f6a90a9f34b4b9649111c68
Matheus Nunes <matheusnunes.sobrinho18@gmail.com>
2023-12-27 09:51:52 -0300

    adicionando dados iniciais

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha) VALUES (1, 'Matheus', 'Rua Rafiki', 25, 'Vila', 'ALUNO', 'matheus@gmail.com', '123456');
+INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha) VALUES (2, 'Binho', 'Rua Simba', 25, 'Cheiroso', 'ALUNO', 'binho@gmail.com', '123456');
+INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha) VALUES (3, 'Junin', 'Rua Mufasa', 25, 'Fedido', 'ALUNO', 'junin@gmail.com', '123456');
+INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha) VALUES (4, 'Igor', 'Rua Kiara', 25, 'Aspero', 'MOTORISTA', 'igor@gmail.com', '123456');
+INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha) VALUES (5, 'Samuel', 'Rua Scar', 25, 'Macio', 'ADM', 'samuel@gmail.com', '123456');
+
+INSERT INTO TB_GEREN_PRESEN (id, id_aluno, data_n_presenca) VALUES (1, 1, '25/12/2023');
