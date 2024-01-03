d83f966b7ad4d49c9c2a67118c9ac692bad7a758
Matheus Nunes <matheusnunes.sobrinho18@gmail.com>
2024-01-03 11:17:01 -0300

    alterando dados iniciais data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,10 @@
 INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha, latitude, longitude) VALUES (1, 'Matheus', 'Rua Rafiki', 25, 'Vila', 'ALUNO', 'matheus@gmail.com', '123456', -20.665963, -43.775913);
-INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha, latitude, longitude) VALUES (2, 'Binho', 'Rua Simba', 25, 'Cheiroso', 'ALUNO', 'binho@gmail.com', '123456', -20.663312, -43.778886);
-INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha, latitude, longitude) VALUES (3, 'Junin', 'Rua Mufasa', 25, 'Fedido', 'ALUNO', 'junin@gmail.com', '123456', -20.663050, -43.784219);
-INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha, latitude, longitude) VALUES (4, 'Igor', 'Rua Kiara', 25, 'Aspero', 'MOTORISTA', 'igor@gmail.com', '123456', -20.661904, -43.784232);
-INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha, latitude, longitude) VALUES (5, 'Samuel', 'Rua Scar', 25, 'Macio', 'ADM', 'samuel@gmail.com', '123456', -20.659444, -43.785291);
-INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha, latitude, longitude) VALUES (6, 'Felipe', 'Rua Pumba', 25, 'Bolinhas', 'ALUNO', 'felipe@gmail.com', '123456', -20.657737, -43.782063);
-INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha, latitude, longitude) VALUES (7, 'Sasuke', 'Rua Sakura', 25, 'Naruto', 'ALUNO', 'sasuke@gmail.com', '123456', -20.654956, -43.781323);
+INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha, latitude, longitude) VALUES (2, 'Binho', 'Rua Simba', 66, 'Cheiroso', 'ALUNO', 'binho@gmail.com', '123456', -20.663312, -43.778886);
+INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha, latitude, longitude) VALUES (3, 'Junin', 'Rua Mufasa', 35, 'Fedido', 'ALUNO', 'junin@gmail.com', '123456', -20.663050, -43.784219);
+INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha, latitude, longitude) VALUES (4, 'Igor', 'Rua Kiara', 120, 'Aspero', 'MOTORISTA', 'igor@gmail.com', '123456', -20.661904, -43.784232);
+INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha, latitude, longitude) VALUES (5, 'Samuel', 'Rua Scar', 88, 'Macio', 'ALUNO', 'samuel@gmail.com', '123456', -20.659444, -43.785291);
+INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha, latitude, longitude) VALUES (6, 'Felipe', 'Rua Pumba', 48, 'Bolinhas', 'ALUNO', 'felipe@gmail.com', '123456', -20.657737, -43.782063);
+INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha, latitude, longitude) VALUES (7, 'Sasuke', 'Rua Sakura', 97, 'Naruto', 'ALUNO', 'sasuke@gmail.com', '123456', -20.654956, -43.781323);
+INSERT INTO TB_USER (id, nome, rua_endereco, num_endereco, bairro_endereco, role, email, senha, latitude, longitude) VALUES (8, 'Izaias', 'Rua Sakura', 155, 'Eliana', 'ADM', 'izaias@gmail.com', '123456', -20.653484, -43.784396);
 
 INSERT INTO TB_GEREN_PRESEN (id, id_aluno, data_n_presenca) VALUES (1, 1, '25/12/2023');
